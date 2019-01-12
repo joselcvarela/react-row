7335a9a6fe0e64c42c53bd901d584455b871c62f
Jose Varela <jvarela@edigma.com>
2019-01-12 12:13:15 +0000

    Update to v1.0.2 *Let user define layout instead of assuming things *Improve layout by re-arranging gutter and defining 12 columns as default

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react'
 import Box from 'react-inln'
 
-const Gutter = ({ width, ...props }) => (
-  <Box flex="1" maxWidth={width} {...props}></Box>
-)
-
 class Row extends Component {
   fillColumns = columns => columns && columns.props ? [columns] : (new Array(12).fill()).reduce((acc, value, i, arr) => {
     if (columns[i+1]) return acc.concat([{ component: columns[i+1], span: 1 }])
     else {
       const latest = acc.slice(-1)[0]
-      latest.span++
+      i > 0 && (latest.span += 1)
       return acc
     }
   }, [{ component: '', span: 1 }])
 
   render() {
     const {
-      columnWidth = '85.33px',
       gutterWidth = '32px',
       children,
       gutterProps,
@@ -28,10 +23,11 @@ class Row extends Component {
     const columns = this.fillColumns(children)
 
     return (
-      <Box display="flex" flexWrap="wrap" flexDirection_s="column" alignItems_s="center" {...props}>
+      <Box display="flex" {...props}>
         { columns.map(({ component, span }, ci) => [
-          <Gutter key={ci+1} width={this.gutterWidth} {...gutterProps} />,
-          <Box key={-ci} flex={span} {...columnProps}>{component}</Box>
+          <Box key={ci} flex={span} paddingLeft={gutterWidth} {...columnProps}>
+            {component}
+          </Box>
         ])}
       </Box>
     )
