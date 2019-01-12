85b9849579134aa3cca64fbaa555b15aa9cfa9d7
Jose Varela <jvarela@edigma.com>
2019-01-12 12:13:15 +0000

    Update to v1.0.2 *Let user define layout instead of assuming things *Improve layout by re-arranging gutter and defining 12 columns as default

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,12 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -32,22 +38,6 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-var Gutter = function Gutter(_ref) {
-  var width = _ref.width,
-      props = _objectWithoutProperties(_ref, ["width"]);
-
-  return _react.default.createElement(_reactInln.default, _extends({
-    flex: "1",
-    maxWidth: width
-  }, props));
-};
 
 var Row =
 /*#__PURE__*/
@@ -74,7 +64,7 @@ function (_Component) {
           span: 1
         }]);else {
           var latest = acc.slice(-1)[0];
-          latest.span++;
+          i > 0 && (latest.span += 1);
           return acc;
         }
       }, [{
@@ -89,33 +79,24 @@ function (_Component) {
   _createClass(Row, [{
     key: "render",
     value: function render() {
-      var _this2 = this;
-
       var _this$props = this.props,
-          _this$props$columnWid = _this$props.columnWidth,
-          columnWidth = _this$props$columnWid === void 0 ? '85.33px' : _this$props$columnWid,
           _this$props$gutterWid = _this$props.gutterWidth,
           gutterWidth = _this$props$gutterWid === void 0 ? '32px' : _this$props$gutterWid,
           children = _this$props.children,
           gutterProps = _this$props.gutterProps,
           columnProps = _this$props.columnProps,
-          props = _objectWithoutProperties(_this$props, ["columnWidth", "gutterWidth", "children", "gutterProps", "columnProps"]);
+          props = _objectWithoutProperties(_this$props, ["gutterWidth", "children", "gutterProps", "columnProps"]);
 
       var columns = this.fillColumns(children);
       return _react.default.createElement(_reactInln.default, _extends({
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection_s: "column",
-        alignItems_s: "center"
-      }, props), columns.map(function (_ref2, ci) {
-        var component = _ref2.component,
-            span = _ref2.span;
-        return [_react.default.createElement(Gutter, _extends({
-          key: ci + 1,
-          width: _this2.gutterWidth
-        }, gutterProps)), _react.default.createElement(_reactInln.default, _extends({
-          key: -ci,
-          flex: span
+        display: "flex"
+      }, props), columns.map(function (_ref, ci) {
+        var component = _ref.component,
+            span = _ref.span;
+        return [_react.default.createElement(_reactInln.default, _extends({
+          key: ci,
+          flex: span,
+          paddingLeft: gutterWidth
         }, columnProps), component)];
       }));
     }
